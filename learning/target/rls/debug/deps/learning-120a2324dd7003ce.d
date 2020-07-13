b1778b805369c39d67a28adeef34e698fb57430a
Micah <Micahsng@gmail.com>
2020-07-13 03:37:08 -0700

    setup

---
+++ learning/target/rls/debug/deps/learning-120a2324dd7003ce.d
@@ -0,0 +1,5 @@
+c:\Users\spide\Documents\GitHub\Rust-Projects\learning\target\rls\debug\deps\learning-120a2324dd7003ce.rmeta: src\main.rs
+
+c:\Users\spide\Documents\GitHub\Rust-Projects\learning\target\rls\debug\deps\learning-120a2324dd7003ce.d: src\main.rs
+
+src\main.rs:
---
+++ learning/target/rls/debug/deps/learning-b4ae9d1aea606ce4.d
@@ -0,0 +1,5 @@
+c:\Users\spide\Documents\GitHub\Rust-Projects\learning\target\rls\debug\deps\learning-b4ae9d1aea606ce4.rmeta: src\main.rs
+
+c:\Users\spide\Documents\GitHub\Rust-Projects\learning\target\rls\debug\deps\learning-b4ae9d1aea606ce4.d: src\main.rs
+
+src\main.rs:
