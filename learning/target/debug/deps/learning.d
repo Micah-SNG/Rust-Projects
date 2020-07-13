6b5ea06d11b1a9df5e46c275e21f28a99a99e20c
Micah <Micahsng@gmail.com>
2020-07-13 02:35:06 -0700

    initial file creation

---
+++ learning/target/debug/deps/learning.d
@@ -0,0 +1,5 @@
+C:\Users\spide\Documents\GitHub\Rust-Projects\learning\target\debug\deps\learning.exe: src\main.rs
+
+C:\Users\spide\Documents\GitHub\Rust-Projects\learning\target\debug\deps\learning.d: src\main.rs
+
+src\main.rs:
---
+++ learning/target/debug/learning.d
@@ -0,0 +1,1 @@
+C:\Users\spide\Documents\GitHub\Rust-Projects\learning\target\debug\learning.exe: C:\Users\spide\Documents\GitHub\Rust-Projects\learning\src\main.rs
